44f3f68f165f420301513bfff7fa7c8ad891bc22
Mahmoud12f <z01116132398a@gmail.com>
2026-02-03 00:10:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; 
+import Home from "./Components/Home/Home";
+import NotFound from "./Components/NotFound/NotFound";
+
+import Layout from "./Components/Layout/Layout"
+import Blog from "./Components/Blog/Blog";
+import Metrics from "./Components/Metrics/Metrics";
+import Values from "./Components/Values/Values";
+
+
+
+
+const myRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, 
+    children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+      { path: "*", element: <NotFound /> },
+      {path: "Blog" , element :<Blog/>},
+      {path: "Metrics" , element:<Metrics/>},
+
+    ],
+  },
+]);
+
+export default function App() {
+
+  return <RouterProvider router={myRouter} />;
+}
